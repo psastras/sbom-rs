43e468ff4fc2c2b3363e93e7f77e04933b2e9868
Paul <psastras@gmail.com>
2023-07-10 22:17:19 +0900

    fix: clippy error

--- serde-cyclonedx/src/cyclonedx.rs
+++ serde-cyclonedx/src/cyclonedx.rs
@@ -4,6 +4,7 @@ pub mod v_1_4 {
   include!(concat!(env!("OUT_DIR"), "/cyclonedx_1_4.rs"));
 }
 
+#[allow(clippy::large_enum_variant)]
 pub mod v_1_5 {
   include!(concat!(env!("OUT_DIR"), "/cyclonedx_1_5.rs"));
 }
