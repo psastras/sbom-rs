cf4f0996528e20a3079486e4aa7af5ab20239138
Paul Sastrasinh <psastras@gmail.com>
2025-06-16 08:30:59 +0900

    fix: clippy

--- serde-cyclonedx/src/cyclonedx.rs
+++ serde-cyclonedx/src/cyclonedx.rs
@@ -1,15 +1,14 @@
-#![allow(clippy::derive_partial_eq_without_eq, clippy::empty_docs)]
-
+#[allow(clippy::all)]
 pub mod v_1_4 {
   include!(concat!(env!("OUT_DIR"), "/cyclonedx_1_4.rs"));
 }
 
-#[allow(clippy::large_enum_variant)]
+#[allow(clippy::all)]
 pub mod v_1_5 {
   include!(concat!(env!("OUT_DIR"), "/cyclonedx_1_5.rs"));
 }
 
-#[allow(clippy::large_enum_variant)]
+#[allow(clippy::all)]
 pub mod v_1_6 {
   include!(concat!(env!("OUT_DIR"), "/cyclonedx_1_6.rs"));
 }
--- serde-spdx/src/spdx.rs
+++ serde-spdx/src/spdx.rs
@@ -1,5 +1,4 @@
-#![allow(clippy::derive_partial_eq_without_eq)]
-
+#[allow(clippy::all)]
 pub mod v_2_3 {
   include!(concat!(env!("OUT_DIR"), "/spdx_2_3.rs"));
 }
